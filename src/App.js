11a393a5417f41efc03b0c65569bdbe2756ce4a6
sunilale0 <sunilale0@gmail.com>
2019-07-08 17:16:40 -0400

    displayed all shapes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Shape from './Shape';
 
-function App() {
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Selector extends Component{
+
+  state = {
+    name: ''
+  }
+
+
+
+  changeText = (event) => {
+
+    console.log("changeText: " + event.target.shape)
+    console.log('changetext: ' + event.target.value)
+    let shape = '';
+    if (event.target.shape === 'square') {shape = 'Square'};
+    if (event.target.shape === 'circle') shape = 'Circle';
+    if (event.target.shape === 'triangle') shape = 'Triangle';
+    this.setState({
+      name: shape
+    })
+    
+  }
+
+  render() {
+
+
+
+    return (
+      <div className='container'>
+        <nav className='navbar'>
+          <h1>
+            Selected: {this.state.name}
+          </h1>
+        </nav>
+
+        <div className= 'shape-list'>
+          {/* <Shape alt="Square" onClick={this.changeText} value='square'/>
+          <Shape alt="Circle" onClick={this.changeText} value='circle'/>
+          <Shape alt="Triangle" onClick={this.changeText} value='triangle'/> */}
+
+          <Shape shape='square' onClick={this.changeText} value = 'square'/>
+        <Shape shape='circle' onClick={this.changeText} value = 'circle'/>
+        <Shape shape='triangle' onClick={this.changeText} value = 'triangle'/>
+        </div>
+
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Selector;
---
+++ src/Shape.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const shape = (props) => {
+
+
+    return (
+        <div className={props.shape} />
+    );
+
+}
+
+export default shape;
